f26aabbfd9e5255a4afda0ff661f6362bae5952d
maxborey <maxborey@gmail.com>
2026-01-31 17:43:06 +0200

    fix auth

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,12 +1,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-const firebaseConfig = { apiKey: "AIzaSyBlVW45OUJnfwhyKZP4aewIrv647F7Iyn0",
-authDomain: "psychologists-services-b403d.firebaseapp.com", projectId:
-"psychologists-services-b403d", storageBucket:
-"psychologists-services-b403d.firebasestorage.app", messagingSenderId:
-"1001258801337", appId: "1:1001258801337:web:1d222ac3d0250fbe3ae368",
-measurementId: "G-5MHHDFM7ZS" };
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
